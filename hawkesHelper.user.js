8a4d7feffed0829507ed981d12342d38b684e4f9
Zaz Brown <zazbrown@zazbrown.com>
2023-05-23 03:16:24 +0000

    put it in proper userscript format
    Also make minor improvements such as spacing, comments.

--- hawkesHelper.user.js
+++ hawkesHelper.user.js
@@ -1,9 +1,19 @@
+// ==UserScript==
+// @name         hawkes-helper
+// @namespace    https://github.com/zaz/hawkes-helper
+// @version      0.1
+// @description  Open a problem in Wolfram Alpha by pressing Alt + q
+// @author       Zaz Brown; https://github.com/zaz/hawkes-helper
+// @include      *://learn.hawkeslearning.com/*
+// @require      https://cdn.jsdelivr.net/npm/@androettop/mathml2latex@1.1.17/lib/mathml2latex.browser.cjs.min.js
+// @grant        none
+// @license      Public Domain
+// ==/UserScript==
+
 // You can copy the line below into the URL bar instead of adding this script to your browser
 // javascript:$.getScript('https://cdn.jsdelivr.net/npm/@androettop/mathml2latex@1.1.17/lib/mathml2latex.browser.cjs.min.js');fixEquation  = latex => latex.replace(/ = $/, '');window.open('https://www.wolframalpha.com/input?i='+$(".MathJax").map((_, e) => $(e).data("mathml")).get().map(mathml2latex.convert).map(fixEquation).join(",  "))
 
-$.getScript('https://cdn.jsdelivr.net/npm/@androettop/mathml2latex@1.1.17/lib/mathml2latex.browser.cjs.min.js')
-
-fixEquation = latex =>
+let fixEquation = latex =>
 	latex.replace(/ = $/, '')
 	     .replace(/\\left/, '')
 	     .replace(/\\right/, '')
@@ -18,8 +28,9 @@ let viewProblemInWolframAlpha = () =>
 	)
 
 $(document).keydown(function (event) {
-		if (event.altKey && event.key === 'q') {
-			viewProblemInWolframAlpha()
-			event.preventDefault()
-		}
+	// if Alt + q is pressed
+	if (event.altKey && event.key === 'q') {
+		viewProblemInWolframAlpha()
+		event.preventDefault()
+	}
 })
